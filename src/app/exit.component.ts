c7d9ceb04aedd3c72bbf86a87d359907fd1fbee0
Sergey Dyatlov <nosaty@mail.ru>
2018-11-28 19:34:05 +0300

    Creating an exit component and style, some fixes in styles

---
+++ src/app/exit.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector:'exit',
+    template:`<h1>До свидания!</h1>`,
+    styleUrls: ['./styles/exit.window.css']
+})
+
+export class ExitComponent{
+
+}
